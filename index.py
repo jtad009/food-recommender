4185ef0646fd07e2ea8604b814f009d082fcbe8d
izzyE <jtad009@gmail.com>
2025-08-13 12:43:41 +0100

    adds hugging face encoder

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 
 import logging
 import os
+from embedder.hugging_face_embedder import HFEmbedder
 from scrappers.all_nigerian_recipe_scraper import AllNigerianRecipesScraper
 from scrappers.base_scrapper import DualSink, JsonArraySink, MongoSink
 from scrappers.yummy_medley_scraper import YummyMedleyScraper
@@ -17,8 +18,10 @@ if __name__ == "__main__":
 
     # Pick a scraper
     # s = AllNigerianRecipesScraper()
-    s = YummyMedleyScraper()
-
+    s = YummyMedleyScraper(
+    )
+    s.embedder = HFEmbedder() 
+    s.embedding_fields = [("ingredients", "ingredients_emb"), ("instructions", "instructions_emb")]
     # Stream with batch writes and resume
     s.stream(
         sink=sink,
